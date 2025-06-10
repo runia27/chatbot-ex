9a8ab8f1215a30280a8cd60201188cc7d654ac7f
runia27 <heywiz86@gmail.com>
2025-06-10 13:49:13 +0900

    refactor: define individual functions for each LLM step in llm.py

--- chat.py
+++ chat.py
@@ -3,32 +3,28 @@ from llm import get_aimessage
 
 
 st.set_page_config(page_title='전세사기피해 상담 챗봇', page_icon='🤖')
-
 st.title('전세사기피해 상담 챗봇 🤖')
-
 
 if 'message_list' not in st.session_state:
     st.session_state.message_list = []
-
-print(f"before: {st.session_state.message_list}")
 
 ## 이전 채팅 화면 출력
 for i in st.session_state.message_list:
     with st.chat_message(i['role']):
         st.write(i['content'])
 
-
-## 프롬프트창(채팅창) 구현
+## 채팅창 및 대화내역 구현 
 if inputchat := st.chat_input(placeholder="전세사기 피해와 관련된 질문을 작성해 주세요."):
     with st.chat_message("user"):    
         st.write(f"{inputchat}")
     st.session_state.message_list.append({'role': 'user', 'content': inputchat})
     
-    aimessage = get_aimessage(inputchat)
-    
-    with st.chat_message("ai"):
-        st.write(aimessage)
-    st.session_state.message_list.append({'role': 'ai', 'content': aimessage})
+    # 답변 나올 때까지 스피너 
+    with st.spinner("답변을 생성하는 중입니다."):
+        aimessage = get_aimessage(inputchat)
+        
+        with st.chat_message("ai"):
+            st.write(aimessage)
+        st.session_state.message_list.append({'role': 'ai', 'content': aimessage})
 
 
-print(f"after: {st.session_state.message_list}")
--- llm.py
+++ llm.py
@@ -8,34 +8,42 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
 
+load_dotenv()
+
+# llm 함수 정의
+def get_llm(model="gpt-4o"):
+    llm = ChatOpenAI(model=model)
+    return llm
 
 
-## AI 메세지 함수 정의 
-def get_aimessage(user_message):
-
-    load_dotenv()
+# 데이터베이스 함수 정의 
+def get_database():
     api_key = os.getenv('PINECONE_API_KEY')
-    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
+    Pinecone(api_key=api_key)
 
-    pc = Pinecone(api_key=api_key)
-
-    ## 임베딩
     embedding = OpenAIEmbeddings(
         model="text-embedding-3-large",
-    )
-
-    # 파인콘에 저장 
-    
+    )    
+   
     database = PineconeVectorStore.from_existing_index(
         embedding=embedding,
-        index_name='law1and2-quiz4'
+        index_name="law1and2-quiz4",
+    )
+    return database
+
+
+# RetrievalQA 함수 정의
+def get_retrievalQA():
+    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
+
+    database = get_database()
+    llm = get_llm()
+    prompt = hub.pull(
+        "rlm/rag-prompt",
+        api_key=LANGCHAIN_API_KEY,
     )
 
-    llm = ChatOpenAI()
 
-    prompt = hub.pull("rlm/rag-prompt")
-
-    ## RetrievalQA 구현
     def format_docs(docs):
         return '\n\n'.join(doc.page_content for doc in docs)
 
@@ -48,10 +56,12 @@ def get_aimessage(user_message):
         | llm
         | StrOutputParser()
     )
+    return chain_lcel
+
+
+## AI 메세지 함수 정의 
+def get_aimessage(user_message):        
+    chain_lcel = get_retrievalQA()
 
     aimessage = chain_lcel.invoke(user_message)
     return aimessage
-
-
-
-
